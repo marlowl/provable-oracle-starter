f08ece6208abce87f83ffed1c9b90fb75f1dccd6
marlow lieshout <marlowlieshout@hotmail.com>
2019-01-05 10:29:32 +0100

    added loading message

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h1>Current ETH price : {{currentValue}} USD</h1>
-    <h1>{{isEqualValue}}</h1>
+    <h1>{{loading}}</h1>
     <button class="button is-primary" @click="setValue">Update Price</button>
   </div>
 </template>
@@ -9,14 +9,14 @@
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
 import Web3 from "web3";
-import { setTimeout } from 'timers';
+import { setTimeout } from "timers";
 const MyContract = require("../../../build/contracts/EthPrice.json");
 @Component
 export default class HelloWorld extends Vue {
   contractValue: string = "";
   currentValue: string = "";
-  isEqualValue: string = "";
   isValueUpdated: boolean = false;
+  loading: string = "";
 
   async setValue() {
     const web3 = new Web3(
@@ -27,22 +27,15 @@ export default class HelloWorld extends Vue {
       "0x345ca3e014aaf5dca488057592ee47305d9b3e10"
     );
 
-    let setValue = await myContract.methods
-      .update()
-      .send({
-        from: "0x627306090abab3a6e1400e9345bc60c78a8bef57",
-        gas: "900000",
-        value: "500000000000000000"
-      })
-    
-    if(setValue)
-    {
-        this.isValueUpdated = true
+    let setValue = await myContract.methods.update().send({
+      from: "0x627306090abab3a6e1400e9345bc60c78a8bef57",
+      gas: "900000",
+      value: "500000000000000000"
+    });
+
+    if (setValue) {
+      this.isValueUpdated = true;
     }
-
-
-    
-
   }
   async getCurrentValue() {
     const web3 = new Web3(
@@ -59,15 +52,14 @@ export default class HelloWorld extends Vue {
       .then(result => {
         this.currentValue = parseFloat(result).toFixed(2);
       });
-
   }
   @Watch("isValueUpdated")
   onPropertyChanged(newValue: boolean, oldValue: boolean) {
     if (newValue == true) {
-      setTimeout(function(){
-           window.location.reload(true);
-      },10000)
-     
+      this.loading = "Price is getting updated....";
+      setTimeout(function() {
+        window.location.reload(true);
+      }, 10000);
     }
   }
   mounted() {
